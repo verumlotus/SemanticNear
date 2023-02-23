0fc7901233d71bb4f378ea88c6851db4fe7ac430
verumlotus <universumtremendum@gmail.com>
2023-02-22 16:17:32 -0800

    feat(server): Deploy on Modal

--- server/server.py
+++ server/server.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from preprocessing import return_relevant_chunks
+import modal
+
+stub = modal.Stub("semantic-near")
+
+mount_exclude_lst = ['.venv', '.mypy_cache', '__pycache__']
+filter_venv = lambda path: False if any([substr in path for substr in mount_exclude_lst]) else True
+mount = modal.Mount.from_local_dir("../server", remote_path = "/", condition=filter_venv)
+image = modal.Image.from_dockerfile("Dockerfile", context_mount=mount)
+
 
 app = FastAPI()
 
@@ -30,3 +39,8 @@ def get_relevant_chunks(text: str, query: str):
     except Exception as e:
         print(e)
         raise e
+
+# This hooks up our asgi fastapi app to modal
+@stub.asgi(image=image, secret=modal.Secret.from_name("openai-secret"))
+def fastapi_app():
+    return app
